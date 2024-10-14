86157a5adcfe04708617676cbc0edb813849038d
grazinascito <grazi.social84@gmail.com>
2024-10-13 22:06:29 -0300

    refactor: Update Supabase anonymous key variable name and add console log

--- lib/services/supabase.ts
+++ lib/services/supabase.ts
@@ -2,3 +2,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON ?? "";
 export const supabase = createClient(supabaseUrl, supabaseKey);
+console.log(supabase);
