eba7c329f18f9df5c524f353eb1d59ffee9b4b6c
grazinascito <grazi.social84@gmail.com>
2024-10-16 19:11:16 -0300

    refactor: Remove unused TaskList component and add Progress component
    - Remove the unused TaskList component from the codebase.
    - Add a new Progress component to the UI components.

--- lib/services/sessions.ts
+++ lib/services/sessions.ts
@@ -1,60 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-"use client";
+import { v4 as uuidv4 } from "uuid";
 
-import { supabase } from "./supabase"; 
-interface Session {
-  id: string;
-  host_user_id: string;
-  session_title: string;
-  start_time?: string;
-  end_time?: string;
-}
-
-// Participant interface
-interface Participant {
-  id: string;
-  session_id: string;
-  user_id: string;
-  role: "host" | "guest";
-  joined_at?: string;
-}
-
-export const createSession = async (
-  hostUserId: string,
-  sessionTitle: string
-): Promise<Session | null> => {
-  try {
-    const { data, error } = await supabase
-      .from("sessions")
-      .insert([{ host_user_id: hostUserId, session_title: sessionTitle }]);
-
-    if (error) throw error;
-
-    // if (data && data.length > 0) {
-    //   // Add the host to the participants table
-    //   await addParticipant(data[0].id, hostUserId, "host");
-    //   return data[0];
-    // }
-    return null;
-  } catch (error) {
-    console.error("Error creating session:", error);
-    return null;
-  }
-};
-export const addParticipant = async (
-  sessionId: string,
-  userId: string,
-  role: "host" | "guest" = "guest"
-): Promise<Participant | null> => {
-  try {
-    const { data, error } = await supabase
-      .from("participants")
-      .insert([{ session_id: sessionId, user_id: userId, role }]);
-
-    if (error) throw error;
-    return data ? data[0] : null;
-  } catch (error) {
-    console.error("Error adding participant:", error);
-    return null;
-  }
+export const createSession = () => {
+  const sessionId = uuidv4();
+  // Add logic to store or use this session ID as needed
+  console.log(`Session created with ID: ${sessionId}`);
+  return sessionId;
 };
